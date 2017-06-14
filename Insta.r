4a7999776c5ef3319c78c83122c5abb2e8c04364
pillutlapramod@gmail.com <pillutlapramod@gmail.com>
2017-06-14 04:03:25 -0400

    Updated data merge and plotting of variables

--- Insta.r
+++ Insta.r
@@ -5,27 +5,62 @@ library(ggplot2)
 library(knitr)
 library(stringr)
 library(DT)
-aisle1 <- read.csv('aisles.csv',stringsAsFactors = F)
-depart1 <- read.csv("departments.csv",stringsAsFactors = F)
-order1 <- read.csv("orders.csv",stringsAsFactors = F,nrows = 1000000)
-product1 <- read.csv("products.csv",stringsAsFactors = F)
+library(plyr)
+library(plotly)
+library(MASS)
+aisle <- read.csv('aisles.csv',stringsAsFactors = F)
+depart <- read.csv("departments.csv",stringsAsFactors = F)
+order <- read.csv("orders.csv",stringsAsFactors = F,nrows = 1000000)
+product <- read.csv("products.csv",stringsAsFactors = F)
 prod_prior <- read.csv("order_products__prior.csv",stringsAsFactors = F,nrows = 1000000)
 prod_train <- read.csv("order_products__train.csv",stringsAsFactors = F)
 
+#Merge the data 
+mydata <- merge(product,depart)
 
-#mydata <- merge(product1,depart1)
+mydata1 <- merge(mydata,aisle)
 
-mydata1 <- merge(mydata,aisle1)
-
-table(order1$eval_set)
+table(order$eval_set)
 colnames(mydata1)
-#prod1, aisle1, departments 
+
 #merge  with the order_prior details."Meaning merge mydata1 with prod_prior df"  
-#order_id,	add_to_cart_order ,	reordered,	product_name	
 
-unique(mydata1$product_name)
+mydata2 <- merge(mydata1,prod_prior)
+colnames(mydata2)
+
+#Taking a small subset of the data 
+sample_data <- (mydata2[1:1000,])
+sample_random <- sample_data[sample(nrow(sample_data), 500), ]
+
+#Viz + Analysis 
+#Fucntion init
+#'Converts data to factor/table format for easy plotting'
+# Plotting the data with respect to the variable
+
+var_freq <- function(x){
+data_x <- as.data.frame(table(x))
+head(data_x)
+ggplot(data = data_x,aes(x = data_x$x,y = data_x$Freq)) + geom_point()
+}
+
+# Viz plots  Analysis and deducing the inferences by plotting them 
+var_freq(sample_random$department)
+#Produce department has the highest order
+
+var_freq(sample_random$product_id)
+#Product Id 45 has been the most purchased 
+
+var_freq(sample_random$aisle_id)
+#Aisle 83 has the item which has the highest order 
+
+var_freq(sample_random$department_id)
+
+var_freq(sample_random$aisle)
+
+var_freq(sample_random$department)
+
+var_freq(sample_random$add_to_cart_order)
 
 
-product_name <- as.data.frame(table(mydata1$product_name))
-
+var_freq(sample_random$reordered)
 
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+Author  : Pramod 
+
+
+The insta.r file is a open project for instacart competition hosted by Kaggle 
+Here we try to predict the items which will be reordered.
+Given the data frames which consist of different variables, we try to predict whether the customer will reorder the item  which he had ordered. 
+
+#Reference kernels from kaggle 
+"https://www.kaggle.com/c/instacart-market-basket-analysis/kernels"
+
